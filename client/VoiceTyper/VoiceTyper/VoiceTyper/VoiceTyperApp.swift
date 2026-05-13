4cb06bd70ae64d1efa6705794d3aacfdb6cc7218
vklusov <werewolf@tut.by>
2026-05-13 20:49:10 +0300

    📱 Client: working recording + hotkey (Right Cmd)
    - AppDelegate with flagsChanged for Right Cmd
    - AudioRecorder using native mic format (44.1kHz Float32)
    - VoiceTyperClient for server communication
    - PasteManager for text insertion
    - Working status bar icon + popover
    - Remaining: network sandbox fix + audio format conversion

---
+++ client/VoiceTyper/VoiceTyper/VoiceTyper/VoiceTyperApp.swift
@@ -0,0 +1,17 @@
+//
+//  VoiceTyperApp.swift
+//  VoiceTyper
+//
+//  Created by VADIM KLUSOV on 13.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct VoiceTyperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- client/VoiceTyper/VoiceTyperApp.swift
+++ client/VoiceTyper/VoiceTyperApp.swift
@@ -5,6 +5,14 @@ struct VoiceTyperApp: App {
     @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
     var body: some Scene {
+        WindowGroup {
+            EmptyView()
+                .frame(width: 0, height: 0)
+                .hidden()
+        }
+        .windowResizability(.contentSize)
+        .windowStyle(.hiddenTitleBar)
+        
         Settings {
             SettingsView()
         }
